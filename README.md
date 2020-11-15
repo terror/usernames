0a525b853a70e46f89abecf2b485aa1dcb452a69
Liam <terroridk@gmail.com>
2020-11-14 20:16:16 -0500

    init

---
+++ README.md
@@ -0,0 +1,26 @@
+## usernames
+
+a list of the most likely potential inactive usernames on github that possess
+a `rare` username.
+
+As per GitHub's official Name Squatting Policy:
+
+> GitHub prohibits account name squatting, and account names may not be reserved or inactively held for future use. Accounts violating this name squatting policy may be removed or renamed without notice. Attempts to sell, buy, or solicit other forms of payment in exchange for account names are prohibited and may result in permanent account suspension.
+
+So feel free to try and claim what you like!
+
+### Checks
+- No starred repositories
+- No repositories
+- No private contributions
+- No following
+- No public contributions
+- Created in year <= 2011
+
+### Results
+[Wordlist](https://github.com/first20hours/google-10000-english/blob/master/google-10000-english-no-swears.txt)  
+[Output](https://github.com/terror/usernames/blob/master/output.txt)
+
+### Note
+*Not all activity on GitHub is publicly available, therefore users scraped here may still be active.*
+
---
+++ main.py
@@ -0,0 +1,94 @@
+#!/usr/bin/env python3
+import requests
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+headers = {"Authorization": os.getenv("TOKEN")}
+
+
+def main():
+    users = open("users.txt", "r")
+    output = open("output2.txt", "w")
+
+    for user in users:
+        res = build_query(user.rstrip())["data"]
+
+        # not a github user
+        if res["user"] is None:
+            continue
+
+        # take older accounts
+        if int(res["user"]["createdAt"][0:4]) > 2011:
+            continue
+
+        curr_user = []
+
+        curr_user.append(res["user"]["starredRepositories"]["totalCount"])
+        curr_user.append(res["user"]["repositories"]["totalCount"])
+        curr_user.append(
+            res["user"]["contributionsCollection"]["totalCommitContributions"]
+        )
+        curr_user.append(
+            res["user"]["contributionsCollection"]["restrictedContributionsCount"]
+        )
+        curr_user.append(res["user"]["following"]["totalCount"])
+        curr_user.append(
+            0
+            if res["user"]["contributionsCollection"]["hasAnyContributions"] is False
+            else 1
+        )
+
+        if sum(curr_user) >= 1:
+            continue
+
+        print("OK!\nUser: {}".format(user))
+        output.write("{}\n".format(user))
+
+
+def run_query(query, variables):
+    request = requests.post(
+        "https://api.github.com/graphql",
+        json={"query": query, "variables": variables},
+        headers=headers,
+    )
+    if request.status_code == 200:
+        return request.json()
+    else:
+        raise Exception(
+            "Query failed to run by returning code of {}. {}".format(
+                request.status_code, query
+            )
+        )
+
+
+def build_query(user):
+    query = """
+    query userInfo($login: String!) {
+            user(login: $login) {
+              name
+              login
+              createdAt
+              starredRepositories {
+                totalCount
+              }
+              repositories {
+                totalCount
+              }
+              following {
+                totalCount
+              }
+              contributionsCollection {
+                totalCommitContributions
+                restrictedContributionsCount
+                hasAnyContributions
+              }
+            }
+        }"""
+
+    variables = {"login": user}
+    return run_query(query, variables)
+
+
+if __name__ == "__main__":
+    main()
